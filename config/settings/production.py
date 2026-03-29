33fbd9a7c2bca90b0586116976cc608663c4d992
444Pipe <delgadofelipe315@gmail.com>
2026-03-29 18:30:25 -0500

    Use DATABASE_URL with SSL for PostgreSQL in production

--- config/settings/production.py
+++ config/settings/production.py
@@ -12,8 +12,9 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
+        ssl_require=True,
     )
 }
 
