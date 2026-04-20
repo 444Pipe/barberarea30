c29c716c6ac4c38090d638501837d393332010e5
444Pipe <delgadofelipe315@gmail.com>
2026-04-19 19:15:38 -0500

    fix: resolve collectstatic concurrency issue during gunicorn startup

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,14 +7,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       os.environ.get('DJANGO_SETTINGS_MODULE',
                                      'config.settings.production'))
 
-# FORZAR MIGRACIONES EN TIEMPO DE EJECUCIÓN (RUNTIME)
-# Esto soluciona el bug crítico de Railway donde DATABASE_URL no está disponible 
-# durante la fase de release (build), causando que las migraciones vayan a SQLite.
-if os.environ.get('DATABASE_URL'):
-    print("⏳ Aplicando migraciones directamente en Postgres durante arranque WSGI...")
-    os.system('python manage.py migrate --noinput')
-    os.system('python manage.py collectstatic --noinput')
-    os.system('python seed.py')
-    print("✅ Migraciones de arranque finalizaron.")
-
 application = get_wsgi_application()
